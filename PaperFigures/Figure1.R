b4d923622e13b25fe99cb7efcbd55af29ac11c04
eraslan <eraslan@in.tum.de>
2019-01-09 22:15:59 -0500

    Figure 1 B,C are submitted

--- PaperFigures/Figure1.R
+++ PaperFigures/Figure1.R
@@ -1,6 +1,94 @@
   source('./Conf.R')
   source("./Main.R")
   
+  TissueAcro = c("Ad", "Ap", "Br", "Co", "Du", "En", "Es", "FT", "Fa", "GB", "He", "Ki", "Li", "Lu", "Ly", "Ov", "Pa", "Pl","Pr", "Re", "SG", "SI", "SM", "Sp", "St", "Te", "Th", "To", "UB")
+  names(TissueAcro) = TissueNames
+  
+  RNA_Protein_Combined <- readRDS(paste0(tempFileDir, "RNA_Protein_CombinedWithFeatures.Rds"))
+  mRNA <- as.matrix(RNA_Protein_Combined[,paste0(TissueNames,"_medianExon")])
+  protein <- as.matrix(RNA_Protein_Combined[,paste0(TissueNames,"_iBAQ")])
+  
+  ## nested linear model
+  ## P: matrix of proteins - or centered version of it
+  ## P: matrix of mRNAs - or centered version of it
+  nested_lm_res = function(R,P){
+    as.data.table(t(
+      sapply(
+        1:ncol(P),
+        function(j){
+          y = P[,j]
+          ## make sure to work with the same dataset for both fits for the test.
+          any_na = is.na(y) | apply(is.na(R), 1, any) 
+          y = P[!any_na,j]
+          x = R[!any_na,j]
+          x_rest = R[!any_na,-j]
+          
+          fit_single <- lm(y ~ x)
+          fit_all <- lm(y ~ x + x_rest)
+          p.value <- anova(fit_single, fit_all, test="Chisq")[["Pr(>Chi)"]][2]
+          
+          c( 
+            r2_single_tissue = summary(lm(P[,j] ~ R[,j]))$adj.r.squared,
+            r2_all_tissues = summary(lm(P[,j] ~R))$adj.r.squared,
+            p.value = p.value
+          )
+        }
+      )
+    ))
+  }
+  
+  ##### Figure 1B
+  
+  abs_vals_res = nested_lm_res(mRNA, protein)
+  abs_vals_res[ ,Tissue:=TissueAcro[TissueNames]]
+  
+  abs_vals_res[which.min(r2_single_tissue)]
+  abs_vals_res[which.max(r2_single_tissue)]
+  
+  
+  abs_vals_res[which.min(r2_all_tissues)]
+  abs_vals_res[which.max(r2_all_tissues)]
+  
+  Figure1B <- ggplot(abs_vals_res, aes(x=r2_single_tissue, y=r2_all_tissues))+
+    geom_point(size=0.5, col="red")+
+    geom_text_repel(aes(label=Tissue),
+                    size=4)+
+    theme(panel.spacing = unit(0, "lines"), 
+          axis.text.x = element_text(size=12, hjust = 1, vjust=0.5), 
+          axis.text.y = element_text(size=12, hjust = 1, vjust=0.5), 
+          plot.title = element_text(size=12, hjust = 0.5), 
+          legend.position = "None", strip.text = element_text(size=15))+
+    geom_abline(intercept = 0, slope = 1)+
+    xlim(c(0.2,0.5))+
+    ylim(c(0.35,0.6))+
+    xlab("Proportion of variance\n explained by mRNA of same tissue")+
+    ylab("Proportion of variance\n explained by mRNA across tissues")
+
+  
+  ##### Figure 1C
+  ## same for centered protein and mRNA levels
+  
+  mRNA_centered <- mRNA - rowMedians(mRNA, na.rm=TRUE)
+  protein_centered <- protein - rowMedians(protein, na.rm=TRUE)
+  rel_vals_res = nested_lm_res(mRNA_centered, protein_centered)
+  rel_vals_res[ ,Tissue:=TissueAcro[TissueNames]]
+  
+  fig1C <- ggplot(rel_vals_res, aes(x=r2_single_tissue, y=r2_all_tissues))+
+    geom_point(size=0.5, col="red")+
+    geom_text_repel(aes(label=Tissue),
+                    size=4)+
+    theme(panel.spacing = unit(0, "lines"), 
+          axis.text.x = element_text(size=12, hjust = 1, vjust=0.5), 
+          axis.text.y = element_text(size=12, hjust = 1, vjust=0.5), 
+          plot.title = element_text(size=12, hjust = 0.5), 
+          legend.position = "None", strip.text = element_text(size=15))+
+    geom_abline(intercept = 0, slope = 1)+
+    xlim(c(0,0.6))+
+    ylim(c(0,0.6))+
+    xlab("Proportion of variance\n explained by mRNA of same tissue")+
+    ylab("Proportion of variance\n explained by mRNA across tissues")
+  
+  
   ##### Figure 1D  : range difference of mRNA and protein levels
   
   RNA_Protein_Combined <- readRDS(paste0(tempFileDir, "RNA_Protein_CombinedWithFeatures.Rds"))
